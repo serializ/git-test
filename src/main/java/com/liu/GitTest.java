e56fb64c7fd4af2430f523696f615ed50d56c838
liuhao <2453911659@qq.com>
2023-01-11 16:57:26 +0800

    hello liuhao!

--- src/main/java/com/liu/GitTest.java
+++ src/main/java/com/liu/GitTest.java
@@ -3,5 +3,7 @@ package com.liu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello world!");
+        System.out.println("hello liuhao!");
     }
 }
